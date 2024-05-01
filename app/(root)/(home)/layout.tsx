3785ecb4fa0aa152d924a38af6c70be891637867
Abhinav Borah <imabhinavborah@gmail.com>
2024-05-01 13:35:01 +0530

    add metadata

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import React, { ReactNode } from "react";
 import Navbar from "@/components/ui/Navbar";
 import Sidebar from "@/components/ui/Sidebar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Loom",
+  description: "A video conference platform",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
